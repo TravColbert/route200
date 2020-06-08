aebb3496db7a3094141a98303712cbcebc450954
Trav Colbert <trav.colbert@gmail.com>
2020-06-08 02:05:59 +0000

    re-arranged items in admin view

--- views/admin.php
+++ views/admin.php
@@ -1,11 +1,11 @@
-<div class="container">
+<div class="container main grid-sm">
   <div class="columns">
     <div class="column col-12">
       <article id="admin-home">
         <div id="admin-domains"></div>
         <div id="admin-users"></div>
-        <div id="admin-statuses"></div>
         <div id="admin-cities"></div>
+        <div id="admin-statuses"></div>
         <div id="admin-territories"></div>
         <div id="admin-app-settings"></div>
       </article>
