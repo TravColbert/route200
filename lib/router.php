71eb63a2e1a94805dbd22035e1b33b1f74983182
FHD System User <fhd@ip-172-31-39-215.us-west-2.compute.internal>
2020-11-08 04:46:38 +0000

    check for empty urlBase property

--- lib/router.php
+++ lib/router.php
@@ -52,7 +52,8 @@ Class Router {
 
   private function getRequestUri() {
     $path = $_SERVER['REQUEST_URI'];
-    if(strpos($path,$this->urlBase)==0) $path = substr($path,strlen($this->urlBase));
+    syslog(LOG_ERR,"this->urlBase: '{$this->urlBase}'");
+    if($this->urlBase && strpos($path,$this->urlBase)==0) $path = substr($path,strlen($this->urlBase));
     syslog(LOG_INFO,"REQUEST_URI: " . $path);
     return $path;
   }
